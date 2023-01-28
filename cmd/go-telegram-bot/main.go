c7b3632c44fb2161074de48259bbd60d3c21be37
galazat <ga.satnav@gmail.com>
2023-01-28 19:17:30 +0300

    Move commands to separate package

--- cmd/go-telegram-bot/main.go
+++ cmd/go-telegram-bot/main.go
@@ -4,6 +4,7 @@ import (
 	"log"
 	"os"
 
+	"github.com/galazat/go-telegram-bot/internal/app/commands"
 	"github.com/galazat/go-telegram-bot/internal/service/product"
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 	"github.com/joho/godotenv"
@@ -29,48 +30,20 @@ func main() {
 
 	productService := product.NewService()
 
+	commader := commands.NewCommander(bot, productService)
+
 	for update := range updates {
 		if update.Message != nil { // If we got a message
 
 			switch update.Message.Command() {
 			case "help":
-				helpCommand(bot, update.Message)
+				commader.Help(update.Message)
 			case "list":
-				listCommand(bot, update.Message, productService)
+				commader.List(update.Message)
 			default:
-				defaultBehavior(bot, update.Message)
+				commader.Default(update.Message)
 			}
 
 		}
 	}
-}
-
-func helpCommand(bot *tgbotapi.BotAPI, inputMessage *tgbotapi.Message) {
-	msg := tgbotapi.NewMessage(inputMessage.Chat.ID,
-		"/help - help\n"+
-			"/list - list product",
-	)
-
-	bot.Send(msg)
-}
-
-func listCommand(bot *tgbotapi.BotAPI, inputMessage *tgbotapi.Message, productService *product.Service) {
-	outputMsgText := "Here all the product: \n\n"
-
-	products := productService.List()
-	for _, p := range products {
-		outputMsgText += p.Title
-		outputMsgText += "\n"
-	}
-	msg := tgbotapi.NewMessage(inputMessage.Chat.ID, outputMsgText)
-
-	bot.Send(msg)
-}
-
-func defaultBehavior(bot *tgbotapi.BotAPI, inputMessage *tgbotapi.Message) {
-	log.Printf("INFO: [%s] %s", inputMessage.From.UserName, inputMessage.Text)
-
-	msg := tgbotapi.NewMessage(inputMessage.Chat.ID, "Your test is:"+inputMessage.Text)
-
-	bot.Send(msg)
 }
---
+++ internal/app/commands/help.go
@@ -0,0 +1,12 @@
+package commands
+
+import tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+
+func (c *Commander) Help(inputMessage *tgbotapi.Message) {
+	msg := tgbotapi.NewMessage(inputMessage.Chat.ID,
+		"/help - help\n"+
+			"/list - list product",
+	)
+
+	c.bot.Send(msg)
+}
